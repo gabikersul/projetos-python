d1c573f5d57bf231ce30f10a13ca85ee544d4d52
gabikersul <gabriela.kersul@hotmail.com>
2020-12-02 18:42:59 -0300

    arruma main

--- Aula10/main.py
+++ Aula10/main.py
@@ -1,9 +1,6 @@
-from Veiculo import Veiculo
-from Bicicleta import Bicicleta
-from Automovel import Automovel
 from Carro import Carro
+from Bicicleta import Bicicleta
 from Moto import Moto
-
 
 motoVerdinha = Moto("Kawasaki", 2, "Verdinho", 2.0, True)
 biciVermelhinha = Bicicleta("usada", 2, "Vermelhinha", 9, True)
